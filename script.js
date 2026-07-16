0cb33e2f2a41dfbe689ef2d3df9493831e1a4b39
Илья Тетерин <45710701+iteterin@users.noreply.github.com>
2026-07-16 08:09:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('section');
-
     function checkVisibility() {
         const triggerBottom = window.innerHeight / 5 * 4;
-
         sections.forEach(section => {
             const sectionTop = section.getBoundingClientRect().top;
-
-            if(sectionTop < triggerBottom) {
-                section.classList.add('visible');
-            } else {
-                section.classList.remove('visible');
-            }
+            if(sectionTop < triggerBottom) { section.classList.add('visible'); } 
+            else                           { section.classList.remove('visible'); }
         });
     }
 
@@ -27,6 +21,4 @@ async function fetchGitHubData() {
 }
 
 fetchGitHubData();
-
-
 /*============================================================================ */
