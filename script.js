d743f5c7c60f85e0316837ce171249a134532609
Илья Тетерин <45710701+iteterin@users.noreply.github.com>
2026-07-16 07:55:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Получаем язык браузера
-    const userLang = navigator.language || navigator.userLanguage;
+    //const userLang = navigator.language || navigator.userLanguage;
 
     // Проверяем, начинается ли язык с "ru"
-    if (!userLang.startsWith("ru")) {
+    //if (!userLang.startsWith("ru")) {
         // Переадресуем на другую страницу
-        window.location.href = "index-en.html"; // Замените на необходимый URL
-    }
+        //window.location.href = "index-en.html"; // Замените на необходимый URL
+    //}
 });
 
 document.addEventListener('DOMContentLoaded', function() {
